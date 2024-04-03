a7a5298038584153415b0712cf1f07b7e5e8d858
Gulnaz <gulnazsahapova1998@gmail.com>
2024-04-04 00:14:30 +0300

    update db_script.py

--- db_script.py
+++ db_script.py
@@ -5,7 +5,7 @@ async def get_quiz_index(user_id):
     # Подключаемся к базе данных
     async with aiosqlite.connect(DB_NAME) as db:
         # Получаем запись для заданного пользователя
-        async with db.execute('SELECT question_index, correct_answers FROM quiz_state WHERE user_id = (?,?,?)', (user_id,)) as cursor:
+        async with db.execute('SELECT question_index, correct_answers FROM quiz_state WHERE user_id = (?)', (user_id,)) as cursor:
             # Возвращаем результат
             results = await cursor.fetchone()
             if results is not None:
